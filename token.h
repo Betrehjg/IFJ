551f26ee2329184bf2aa767944871bd288e0b9b7
Matej Dubec <33006703+emsiD@users.noreply.github.com>
2019-12-12 00:04:06 +0100

    Update token.h

--- token.h
+++ token.h
@@ -28,19 +28,19 @@ typedef enum {
 
 } t_state;
 
+// TYPY TOKENU
 static const char *token_state_string[] = {"ID", "STR", "DOCSTRING", "INTEGER", "DOUBLE", "LOG_EQ", "LOG_NONEQ", "LOG_MORE", "LOG_LESS,",
                                            "LOG_LEQ", "LOG_MEQ", "LOG_NEQ", "ADD", "SUBSTRACT", "MULTIPLY", "DIVIDE", "IDIVIDE", "EOL", "L_BRAC", "R_BRAC",
                                            "ASSIGN", "COMMA", "LEX_EOF", "COLON", "KW_DEF", "KW_ELSE", "KW_IF", "KW_NONE", "KW_PASS", "KW_RETURN",
                                            "KW_WHILE", "START", "LINE_COMM", "DOCSTR_INPUT", "DOCSTR_BEGIN", "DOCSR_END", "STR_INPUT", "STR_BCKLSG",
                                            "DOCSTR_BCKSLSH", "STR_HEX", "STR_HEX2", "EXPR_T", "EXPR_S", "DOT", "EXCLAIM", "INDENT", "DEDENT"};
 
-/// STRUKTURA TOKEN A OPERACIE NAD NOU !!!
 
-/// STRUKTURA TOKEN A OPERACIE NAD NOU !!!
+/// STRUKTURA TOKENU
 
 typedef struct {
     t_state type;
-    char *data; // v pripade INDENT a DEDENT tak pocet medzier
+    char *data; // V pripade INDENT a DEDENT pocet medzier / inac znakova reprezentacia tokenu
     int data_size;
 } t_token;
 
